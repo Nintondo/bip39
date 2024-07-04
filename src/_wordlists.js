d08b27a21535e410b9cac436f9b5f3c5ae8120cd
Kiel Ed <kieledssh@gmail.com>
2024-07-04 12:48:47 +0300

    init

---
+++ src/_wordlists.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// browserify by default only pulls in files that are hard coded in requires
+// In order of last to first in this file, the default wordlist will be chosen
+// based on what is present. (Bundles may remove wordlists they don't need)
+const wordlists = {};
+exports.wordlists = wordlists;
+let _default;
+exports._default = _default;
+try {
+    exports._default = _default = require('./wordlists/english.json');
+    wordlists.english = _default;
+    wordlists.EN = _default;
+}
+catch (err) { }
